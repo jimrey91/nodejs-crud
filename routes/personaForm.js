1067ba6e4804c70cf24ed9f73d6f088502722b04
Jaime Reyes <jimmy_melendez@hotmail.com>
2019-06-15 11:52:33 -0600

    Ultimos cambios

--- routes/personaForm.js
+++ routes/personaForm.js
@@ -4,6 +4,8 @@ let router = express.Router();
 let mongoose = require('./../config/conexion');
 let Persona = require('./../model/persona');
 
+mongoose.set('useFindAndModify', false);
+
 router.post('/persona/operar', (req, res, next) => {
 
     if(req.body._id === ""){
@@ -14,14 +16,14 @@ router.post('/persona/operar', (req, res, next) => {
         });
         per.save();
     }else{
-        /* funcion depreciada
+        /* funcion depreciada*/
         Persona.findByIdAndUpdate(req.body._id, { $set: req.body }, { new: true }, (err, model) => {
             if (err) throw err;
-        }*/
-        
-        Persona.findOneAndUpdate(req.body._id, { $set: req.body }, { new: true }, (err, model) => {
-            if (err) throw err;
         });
+        
+/*         Persona.findOneAndUpdate(req.body._id, { $set: req.body }, { new: true }, (err, model) => {
+            if (err) throw err;
+        }); */
     }
     res.redirect('/');
 });
