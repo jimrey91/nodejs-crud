5e96078f126c42fcb02d9fab578e4f6d5c99f78f
Jaime Reyes <jimmy_melendez@hotmail.com>
2019-06-14 20:54:22 -0600

    Actualizar conexion
    modificacion uri de conexion en mongodb

--- config/conexion.js
+++ config/conexion.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose');
-let uri = 'mongodb+srv://dbUser:<password>@cluster0-awfjp.mongodb.net/test?retryWrites=true&w=majority'
+let uri = 'mongodb+srv://dbUser:test@cluster0-awfjp.mongodb.net/crud?retryWrites=true&w=majority'
 
 mongoose.connect(uri, {useNewUrlParser: true});
 //'mongodb://localhost:27017/crud'
